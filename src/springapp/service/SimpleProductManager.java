42bbc78846716f093bd45f374dd332b674c3e78c
Rodrigo <lenzirodrigo@gmail.com>
2012-10-31 16:04:25 -0300

    6. Integrating the WebApp with Persistence Layer

--- src/springapp/service/SimpleProductManager.java
+++ src/springapp/service/SimpleProductManager.java
@@ -4,27 +4,38 @@ import java.util.List;
 import java.util.ArrayList;
 
 import springapp.domain.Product;
+import springapp.repository.ProductDao;
+
 
 public class SimpleProductManager implements ProductManager {
 
     private static final long serialVersionUID = 1L;
-    private List<Product> products;
+    // private List<Product> products;
+    private ProductDao productDao;
+
+    public List<Product> getProducts() {
+        // return products;
+        return productDao.getProductList();
+    } 	
+
+	public void increasePrice(int percentage) {
+		List<Product> products = productDao.getProductList();
+		if (products != null) {
+			for (Product product : products) {
+				double newPrice = product.getPrice().doubleValue()
+						* (100 + percentage) / 100;
+				product.setPrice(newPrice);
+				productDao.saveProduct(product);
+			}
+		}
+	}
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
     
-	public List<Product> getProducts() {
-        return products;
-    }
-
-    public void increasePrice(int percentage) {
-        if (products != null) {
-            for (Product product : products) {
-                double newPrice = product.getPrice().doubleValue() * 
-                                    (100 + percentage)/100;
-                product.setPrice(newPrice);
-            }
-        }    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
+//    public void setProducts(List<Product> products) {
+//        this.products = products;
+//    }
 
 }
--- test/springapp/web/InventoryControllerTests.java
+++ test/springapp/web/InventoryControllerTests.java
@@ -1,9 +1,11 @@
 package springapp.web;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.web.servlet.ModelAndView;
-
+import springapp.domain.Product;
+import springapp.repository.InMemoryProductDao;
 import springapp.service.SimpleProductManager;
 import springapp.web.InventoryController;
 
@@ -13,7 +15,11 @@ public class InventoryControllerTests extends TestCase {
 
     public void testHandleRequestView() throws Exception{		
         InventoryController controller = new InventoryController();
-        controller.setProductManager(new SimpleProductManager());
+        
+        SimpleProductManager spm = new SimpleProductManager();
+        spm.setProductDao(new InMemoryProductDao(new ArrayList<Product>()));
+        controller.setProductManager(spm);
+        //controller.setProductManager(new SimpleProductManager());        
         ModelAndView modelAndView = controller.handleRequest(null, null);		
         assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
