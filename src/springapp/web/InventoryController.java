a86e7e64f9ed0d347eba4adaa7503ae6f2b1082e
Rodrigo <lenzirodrigo@gmail.com>
2012-10-31 12:08:18 -0300

    4. Developing the Web Interface

--- src/springapp/web/InventoryController.java
+++ src/springapp/web/InventoryController.java
@@ -12,21 +12,32 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
+import springapp.service.ProductManager;
 
-public class HelloController implements Controller {
+public class InventoryController implements Controller {
 
     protected final Log logger = LogFactory.getLog(getClass());
+    
+    private ProductManager productManager;
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        logger.info("Returning hello view");
-
         String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
+        
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
                 
-        return new ModelAndView("hello", "now", now);
+        return new ModelAndView("hello", "model", myModel);
+    }
+    
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
     }
 
 }
